d791fd7a9447f1186840c293085a14fb311bbf93
Colin Edwards <ColinJhEdwards@gmail.com>
2021-01-30 19:19:06 -0800

    added function that will display searched city main stats on home page

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-var apiKey = "my api key";
+$(document).ready(function () {
+  //global stuff
+  const apiKey = "b5c842f082af7c42b54113072b2a2dc6";
+  const currentDay = moment().format("L");
 
-function getRequestUrl(cityName) {
-  return `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-}
+  function getRequestUrl(cityName) {
+    return `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b5c842f082af7c42b54113072b2a2dc6&units=imperial`;
+  }
+  function requestUvIndex(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b5c842f082af7c42b54113072b2a2dc6&units=imperial`;
+  }
 
-function getWeatherData(cityName) {
-  $.ajax({
-    url: getRequestUrl(cityName),
-    method: "GET",
-  }).then(function (data) {
-    console.log(data);
+  //event listeners
+  $("#searchBtn").on("click", function (e) {
+    e.preventDefault();
+    let cityName = $("#cityInput").val();
+    getWeatherData(cityName);
   });
-}
 
-$("#button").on("click", function () {
-  let cityName = $("#input-id").val();
-  getWeatherData(cityName);
+  //functions
+  function getWeatherData(cityName) {
+    $.ajax({
+      url: getRequestUrl(cityName),
+      method: "GET",
+    }).then(function (data) {
+      console.log(data);
+      $("#location").text(data.name + " " + currentDay);
+      $("#currentTemp").text("Temp: " + data.main.temp);
+      $("#currentHumi").text("Humidity: " + data.main.humidity);
+      $("#currentWS").text("Wind Speed: " + data.wind.speed);
+      const lat = data.coord.lat;
+      const lon = data.coord.lon;
+    });
+  }
 });
